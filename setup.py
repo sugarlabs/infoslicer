354d415e2f835104964c8a5c3f923d55df3987dd
Walter Bender <walter@walter-laptop.(none)>
2009-01-31 10:56:21 -0500

    adding BeautifulSoup dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) IBM Corporation 2008
-# Taken from the OLPC Wiki.
-try:
-    from sugar.activity import bundlebuilder
-    bundlebuilder.start("InfoSlicer")
-except ImportError:
-    import os
-    os.system("find ./ | sed 's,^./,InfoSlicer.activity/,g' > MANIFEST")
-    os.system('rm InfoSlicer.xo')
-    os.chdir('..')
-    os.system('zip -r InfoSlicer.xo InfoSlicer.activity')
-    os.system('mv InfoSlicer.xo ./InfoSlicer.activity')
-    os.chdir('InfoSlicer.activity')
+from sugar.activity import bundlebuilder
+if __name__ == "__main__":
+    bundlebuilder.start()
+
