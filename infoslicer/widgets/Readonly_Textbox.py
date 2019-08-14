9d6894e20c5506f450681f174ebd453e7a24ab8a
nswarup14 <nswarup14@gmail.com>
2019-08-14 14:50:42 +0530

    Revert "Fix traceback caused due to return of bool and remove extra function"
    This reverts commit d5ddc5e08bad3fd4bc8ace913f76d8c55c85acfe.

--- infoslicer/widgets/Readonly_Textbox.py
+++ infoslicer/widgets/Readonly_Textbox.py
@@ -128,32 +128,29 @@ class Readonly_Textbox( Textbox ):
                 buf = self.get_buffer()            
                 mouseiter = self.get_mouse_iter(int(event.x), int(event.y))
                 article = self.get_article()
-                if isinstance(mouseiter, bool):
-                    pass
+                if mouseiter.compare(self.selectionstart) == 1:
+                    if self.selectionmode == SELECT_SENTENCE: 
+                        selectionstart = article.getSentence(self.selectionstart).getStart()
+                        selectionend = article.getSentence(mouseiter).getEnd()
+                    if self.selectionmode == SELECT_PARAGRAPH:
+                        selectionstart = article.getParagraph(self.selectionstart).getStart()
+                        selectionend = article.getParagraph(mouseiter).getEnd()
+                    if self.selectionmode == SELECT_SECTION:
+                        selectionstart = article.getSection(self.selectionstart).getStart()
+                        selectionend = article.getSection(mouseiter).getEnd()
                 else:
-                    if mouseiter.compare(self.selectionstart) == 1:
-                        if self.selectionmode == SELECT_SENTENCE:
-                            selectionstart = article.getSentence(self.selectionstart).getStart()
-                            selectionend = article.getSentence(mouseiter).getEnd()
-                        if self.selectionmode == SELECT_PARAGRAPH:
-                            selectionstart = article.getParagraph(self.selectionstart).getStart()
-                            selectionend = article.getParagraph(mouseiter).getEnd()
-                        if self.selectionmode == SELECT_SECTION:
-                            selectionstart = article.getSection(self.selectionstart).getStart()
-                            selectionend = article.getSection(mouseiter).getEnd()
-                    else:
-                        if self.selectionmode == SELECT_SENTENCE:
-                            selectionstart = article.getSentence(mouseiter).getStart()
-                            selectionend = article.getSentence(self.selectionstart).getEnd()
-                        if self.selectionmode == SELECT_PARAGRAPH:
-                            selectionstart = article.getParagraph(mouseiter).getStart()
-                            selectionend = article.getParagraph(self.selectionstart).getEnd()
-                        if self.selectionmode == SELECT_SECTION:
-                            selectionstart = article.getSection(mouseiter).getStart()
-                            selectionend = article.getSection(self.selectionstart).getEnd()
-                    self.scroll_to_iter(mouseiter, 0, False, 0.5, 0.5)
-                    article.highlight(selectionstart, selectionend)
-
+                    if self.selectionmode == SELECT_SENTENCE: 
+                        selectionstart = article.getSentence(mouseiter).getStart()
+                        selectionend = article.getSentence(self.selectionstart).getEnd()
+                    if self.selectionmode == SELECT_PARAGRAPH:
+                        selectionstart = article.getParagraph(mouseiter).getStart()
+                        selectionend = article.getParagraph(self.selectionstart).getEnd()
+                    if self.selectionmode == SELECT_SECTION:
+                        selectionstart = article.getSection(mouseiter).getStart()
+                        selectionend = article.getSection(self.selectionstart).getEnd()
+                self.scroll_to_iter(mouseiter, 0, False, 0.5, 0.5)
+                article.highlight(selectionstart, selectionend)                
+                    
             else:
                 self.block = True
             
